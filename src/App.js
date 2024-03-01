379de52298a213f7f8ea082d25fe0a4d7f56f808
Arnav Kumar <you@example.com>
2024-02-29 23:08:52 -0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Home from './Pages/Home';
+import Messages from './Pages/Messages';
+import Login from './Pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <Router>
+      
+
+          <Routes>
+          {/*<Route index element={<Login />} /> */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/login" element={<Login />} />
+          
+          </Routes>
+  </Router>
+  </>
+  
   );
 }
 
 export default App;
+
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import '../App.css';
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+
+    const navigate = useNavigate();
+    
+    return (
+
+        <div className="login-background">
+
+            <div className="wrapper-login">
+                <form onSubmit={() => navigate("/home")}>
+                    <img src="https://forestgeo.si.edu/sites/default/files/institutions/logo_-_uc_santa_cruz.png" alt="UCSC"></img>
+                    <div className="input-box">
+                        <input type="text" placeholder="CruzID" required />
+                    </div>
+
+                    <div className="input-box">
+                        <input type="text" placeholder="Password" required />
+                    </div>
+
+                    <div className="forgot">
+                        <a href="#">Forgot Password?</a>
+                    </div>
+            
+                    <button type="submit">Login</button>
+
+                    <hr
+                    style={{
+                    background: "black",
+                    height: "4px",
+                    borderRadius: '16px',
+                    marginTop: '15px',
+                    }}
+                    />
+
+                </form>
+
+            <a href="https://canvas.ucsc.edu/"> 
+                <button style={{
+                    marginTop: '15px',
+                    background: 'black',
+                    color: 'red',
+                }}><img style={{maxWidth:'110px',}}src="https://financesonline.com/uploads/2019/08/canvas-logo1.png" alt="Canvas"></img></button>
+            </a>
+            
+
+        </div>
+    </div>
+        
+    )
+}
